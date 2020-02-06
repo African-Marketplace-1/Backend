e06e93920474611862e135f42d0183331e100e16
fishmanjohn <john@emmaandjohnbyvan.com>
2020-02-06 10:59:43 -0800

    did more testing

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -1,10 +1,12 @@
 const server =require('../server')
 const request = require('supertest')
 const db = require('../dbConfig')
+
 beforeEach(()=>{
     return db.migrate.rollback()
     .then(()=>db.migrate.latest())
-    .then(()=>db.seed.run)
+    .then(()=>db.seed.run())
+   
 })
 
 
@@ -14,11 +16,25 @@ describe('Authorization / authentication', ()=>{
         .post('/api/auth/register')
         .send({username:'pickleRick',password:'jaguar',profile_pic_url: ''})
         expect(res.status).toBe(201)
+        expect(res.body).toHaveProperty('id')
     })
     it('gives response 401 to invalid user', async ()=>{
         const res = await request(server)
         .post('/api/auth/login')
         .send({ username:'fart',password:'goodbyemoonmen' })
         expect(res.status).toBe(401)
+    })
+})
+
+describe('login user',()=>{
+    it('Post /api/auth/login', async ()=>{
+        const register = await request(server)
+        .post('/api/auth/register')
+        .send({username:'pickleRick',password:'jaguar',profile_pic_url: ''})
+        const res = await request(server)
+        .post('/api/auth/login')
+        .send({username:"pickleRick", password:"jaguar"})
+        expect(res.status).toBe(200)
+        expect(res.body.token)
     })
 })
--- listings/listings.spec.js
+++ listings/listings.spec.js
@@ -1,10 +1,21 @@
 const server =require('../server')
 const request = require('supertest')
+const db = require('../dbConfig')
+
+beforeEach(()=>{
+    return db.migrate.rollback()
+    .then(()=>db.migrate.latest())
+    .then(()=>db.seed.run())
+   
+})
 
 describe('avg-price', ()=>{
     it('gets json array of listings', async ()=>{
         const res = await request(server).get('/api/listings')
        .expect('Content-type','application/json; charset=utf-8')
     })
-   
+   it('gets a single listing', async ()=>{
+       const res = await request(server).get('/api/listing/2')
+       .expect('Content-type','text/html; charset=utf-8')
+   } )
 })
